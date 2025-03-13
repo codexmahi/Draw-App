4f7f2298dbad5b4122ecc989050516d59cebf9d8
codexmahi <mahimaupadhyay.code@gmail.com>
2025-03-13 06:22:54 +0000

    completed step 10,11,12

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,12 +1,22 @@
 import { WebSocketServer } from "ws";
-
-const WebSocket = require('ws');
-
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from './config';
 const wss = new WebSocketServer({ port: 8888 });
 
-wss.on('connection', function connection(ws){
+wss.on('connection', function connection(ws,request) {
+  const url = request.url;
+  if (!url) {
+    return;
+  }
+  const queryParams = new URLSearchParams(url.split('?')[1]);
+  const token = queryParams.get('token')|| "";
+  const decoded = jwt.verify(token, JWT_SECRET);
  
-    wss.on('message', function incoming(data) {
+  if (!decoded || !(decoded as JwtPayload).userid) {
+    ws.close();
+    return;
+  }
+    ws.on('message', function incoming(data) {
          ws.send('pong');
   });
 
